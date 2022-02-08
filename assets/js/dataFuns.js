59bcf956da6d93ccc751fb8eee0a69fd35b0f422
th0m45y3 <th0m45y3@gmail.com>
2022-02-08 23:20:37 +0000

    feat: version 5
    top Rated; query search
    toDo: top Rated text; query search < 20pcs
    load icon

--- assets/js/dataFuns.js
+++ assets/js/dataFuns.js
@@ -1,27 +1,68 @@
 const api_key = '219b318ed4c1f1965c4a13ec08a94f4c';
 const lang = 'en-US';
-const posterUrl = 'https://image.tmdb.org/t/p/original/';
+const posterUrl = 'https://image.tmdb.org/t/p/original';
 const table = document.getElementById('big-table');
 
 export async function getData(url, params={'page':1}) {
+    showLoading();
     try {
         const res = await fetch(url + combineQueryParams(params));
         const data = await res.json();
 
-        console.log(data.results.length);
-        if (data.results.length < 20) makeRows(data.results.length);
-        var currentImage = posterUrl + data.results[0]['poster_path'];
-        table.querySelectorAll('.card')[1].querySelector("img").style.backgroundImage =  `url(${currentImage})`;
-        console.log(currentImage);
+        console.log(data.results);
+        displayResults(data.results);
       } catch (err) {
-        table.style.display = 'none';
-        document.getElementById('error-message').classList.remove('hidden');
+        showError();
         throw err;
         return;
     }
 }
+
+function showTable() {
+    document.getElementById('error-message').classList.add('hidden');
+    document.getElementById('loading-message').classList.add('hidden');
+    table.classList.remove('hidden');
+}
+
+function showError() {
+    table.classList.add('hidden');
+    document.getElementById('loading-message').classList.add('hidden');
+    document.getElementById('error-message').classList.remove('hidden');
+}
+
+function showLoading() {
+    table.classList.add('hidden');
+    document.getElementById('loading-message').classList.remove('hidden');
+    document.getElementById('error-message').classList.add('hidden');
+}
+
+function displayResults(data) {
+    showTable();
+    var imageArray = [];
+    //if (data.length < 20) makeRows(data.length);
+    for(var res in data) {
+        table.querySelectorAll('.poster-image').forEach(function(card, index ){
+            const img = new Image();
+            console.log(posterUrl + data[index].poster_path);
+            img.src = String(posterUrl + data[index].poster_path);
+            //img.onload()
+            //card.style.backgroundImage = "url(" + String(posterUrl + data[index].poster_path) + ")";
+            imageArray[index] = img;
+            return;
+         });
+    }
+
+    for(var res in data) {
+        table.querySelectorAll('.poster-image').forEach((card, index ) =>
+            card.style.backgroundImage = "url(" + imageArray[index].src + ")");
+         }
+    }
+
+
 function makeRows(rows) {
-    let tableSize = table.rows.length;
+    let tableSize = table.tBodies[0].rows.length;
+    console.log(table.tBodies[0].rows.length);
+    console.log(table.tBodies[0].rows);
     while(rows < tableSize) {
         table.deleteRow(tableSize-1);
     }
--- assets/js/index.js
+++ assets/js/index.js
@@ -5,8 +5,10 @@ const search = document.getElementById('search');
 const topUrl = 'https://api.themoviedb.org/3/movie/top_rated/';
 const queryUrl = 'https://api.themoviedb.org/3/search/movie';
 
+
 elem.playbackRate = 0.5;
 funs.getData(topUrl);
+
 
 search.onkeydown = function(e){
     if (!e) e = window.event;
