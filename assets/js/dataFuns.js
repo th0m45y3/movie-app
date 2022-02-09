2f5f284fe0b67146184221ba918c950e9b18d038
th0m45y3 <th0m45y3@gmail.com>
2022-02-09 23:30:19 +0000

    feat: version 6
    scrollable info!

--- assets/js/dataFuns.js
+++ assets/js/dataFuns.js
@@ -9,7 +9,7 @@ export async function getData(url, params={'page':1}) {
         const res = await fetch(url + combineQueryParams(params));
         const data = await res.json();
 
-        console.log(data.results);
+        //console.log(data.results);
         displayResults(data.results);
       } catch (err) {
         showError();
@@ -37,26 +37,40 @@ function showLoading() {
 }
 
 function displayResults(data) {
+    resetImages();
+    for(var res in data) {
+        var currentItem = document.getElementById(`card-${res}`)
+        console.log( data[res]);
+        currentItem.querySelector('.title').innerHTML = data[res].title;
+        currentItem.querySelector('.overview').innerHTML = data[res].overview;
+        currentItem.querySelector('.release-date').innerHTML = data[res].release_date;
+
+        table.querySelectorAll('tdbody .info').forEach(function(card, index ){
+            // console.log(card, index);
+            // console.log(data[index].title);
+            //card.querySelector('.title').innerHTML = data[index].title;
+            }
+        )
+    }
     showTable();
-    var imageArray = [];
     //if (data.length < 20) makeRows(data.length);
     for(var res in data) {
         table.querySelectorAll('.poster-image').forEach(function(card, index ){
             const img = new Image();
-            console.log(posterUrl + data[index].poster_path);
-            img.src = String(posterUrl + data[index].poster_path);
-            //img.onload()
-            //card.style.backgroundImage = "url(" + String(posterUrl + data[index].poster_path) + ")";
-            imageArray[index] = img;
+            img.src = data[index].poster_path ? String(posterUrl + data[index].poster_path) : './assets/img/noImage.png';
+            console.log(img.src);
+            img.onload = () => card.style.backgroundImage = "url(\'" + img.src + "\')";
             return;
          });
     }
 
-    for(var res in data) {
-        table.querySelectorAll('.poster-image').forEach((card, index ) =>
-            card.style.backgroundImage = "url(" + imageArray[index].src + ")");
+    // for(var res in data) {
+    //     table.querySelectorAll('.poster-image').forEach((card, index ) =>
+    //         card.style.backgroundImage = imageArray[index]
+    //     )
+            // card.style.backgroundImage = "url(" + imageArray[index].src + ")");
          }
-    }
+
 
 
 function makeRows(rows) {
@@ -70,4 +84,8 @@ function makeRows(rows) {
 
 function combineQueryParams(params){
     return '?api_key='+ api_key + '&' + Object.keys(params).map( k => `${k}=${params[k]}`).join('&');
+}
+
+function resetImages() {
+    table.querySelectorAll('.poster-image').forEach( card => card.style.backgroundImage = 'url(./assets/video/load.gif)')
 }
