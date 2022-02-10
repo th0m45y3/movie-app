0c37823ef830f07305b74f2174beb185cebcca0f
th0m45y3 <th0m45y3@gmail.com>
2022-02-10 16:35:58 +0000

    feat: version 7
    complete poster filter

--- assets/js/dataFuns.js
+++ assets/js/dataFuns.js
@@ -9,8 +9,8 @@ export async function getData(url, params={'page':1}) {
         const res = await fetch(url + combineQueryParams(params));
         const data = await res.json();
 
-        //console.log(data.results);
         displayResults(data.results);
+        console.log(data);
       } catch (err) {
         showError();
         throw err;
@@ -19,18 +19,23 @@ export async function getData(url, params={'page':1}) {
 }
 
 function showTable() {
+    document.body.style.overflow = '';
     document.getElementById('error-message').classList.add('hidden');
     document.getElementById('loading-message').classList.add('hidden');
     table.classList.remove('hidden');
 }
 
 function showError() {
+    window.scrollTo(0, 0);
+    document.body.style.overflow = 'hidden';
     table.classList.add('hidden');
     document.getElementById('loading-message').classList.add('hidden');
     document.getElementById('error-message').classList.remove('hidden');
 }
 
 function showLoading() {
+    window.scrollTo(0, 0);
+    document.body.style.overflow = 'hidden';
     table.classList.add('hidden');
     document.getElementById('loading-message').classList.remove('hidden');
     document.getElementById('error-message').classList.add('hidden');
@@ -40,36 +45,23 @@ function displayResults(data) {
     resetImages();
     for(var res in data) {
         var currentItem = document.getElementById(`card-${res}`)
-        console.log( data[res]);
         currentItem.querySelector('.title').innerHTML = data[res].title;
         currentItem.querySelector('.overview').innerHTML = data[res].overview;
         currentItem.querySelector('.release-date').innerHTML = data[res].release_date;
-
-        table.querySelectorAll('tdbody .info').forEach(function(card, index ){
-            // console.log(card, index);
-            // console.log(data[index].title);
-            //card.querySelector('.title').innerHTML = data[index].title;
-            }
-        )
+        currentItem.querySelector('.popularity').innerHTML = data[res].popularity;
+        currentItem.querySelector('.vote-average').innerHTML = data[res].vote_average;
     }
     showTable();
-    //if (data.length < 20) makeRows(data.length);
+    if (data.length < 20) makeRows(data.length);
     for(var res in data) {
         table.querySelectorAll('.poster-image').forEach(function(card, index ){
             const img = new Image();
             img.src = data[index].poster_path ? String(posterUrl + data[index].poster_path) : './assets/img/noImage.png';
-            console.log(img.src);
             img.onload = () => card.style.backgroundImage = "url(\'" + img.src + "\')";
             return;
          });
     }
-
-    // for(var res in data) {
-    //     table.querySelectorAll('.poster-image').forEach((card, index ) =>
-    //         card.style.backgroundImage = imageArray[index]
-    //     )
-            // card.style.backgroundImage = "url(" + imageArray[index].src + ")");
-         }
+}
 
 
 
